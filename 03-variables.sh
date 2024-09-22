2d7366466b566793be5873f3f78d05d73deff0ec
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-22 21:41:44 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+PERSON1=Nikhitha
+PERSON2=Ruchitha
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good Ruchitha. Whats going on?"
+echo "$PERSON2:: I started learning shell Script $PERSON1"
