d9c4414f46c5eef1c614d027c95aff00694f0163
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-22 21:20:24 +0530

    added conversation script

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Nikhitha:: Hi Ruchitha, How are you?"
+echo "Ruchitha:: Hello Nikhitha, I am fine. How are you doing?"
+echo "Nikhitha:: I am doing good Ruchitha. Whats going on?"
+echo "Ruchitha:: I started learning shell Script Nikhitha"
