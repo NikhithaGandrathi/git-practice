aca49bdaa1dae7b7d7742ab3059a1032f8d50418
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-23 16:37:43 +0530

    functions

---
+++ 10-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1 
+
+if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
+then
+   echo "Given number: $Number is greater than 20"
+
+else
+
+  echo  "Given number: $Number is less than 20"
+fi
