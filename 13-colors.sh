36e7e34c3a8dc9f7d9bd64245ff7347da456b777
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-23 18:44:56 +0530

    functions

---
+++ 13-colors.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+R="\e[31m"
+G="\e[32m"
+G="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0]
+    then
+       echo "Please run this script with root priveleges"
+       exit 1
+   fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2 is...$R FAILED $N"
+    exit 1
+    else
+      echo -e "$2 is...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "Please run this script with root priveleges"
+   exit 1
+fi
+
+dnf list installed git
+
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+       
+else
+    echo "Git is already installed, nothing to do.."
+fi    
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "MySql is not installed, going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySql"
+    
+else
+   echo "MySql is already installed..nothing to do"
+fi
+
