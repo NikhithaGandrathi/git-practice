df5620f9eda6072550855b7c3f6a34b42f915f2a
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-23 16:19:20 +0530

    functions

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,4 +5,6 @@ echo "Number of variables passed: $#" #count
 echo "Script name: $0"
 echo "Current working directory:$PWD"
 echo "Home directory of current user: $HOME"
-echo "PID of the script executing now: $$"
+echo "PID of the script executing now: $$" #PID-Process instance ID
+sleep 100 &
+echo "PID of last background command: $!"
