be0667d4541cb6832fd449f8e75aeb0e279a746c
NikhithaGandrathi <itsnikhithagandrathi@gmail.com>
2024-09-23 16:07:33 +0530

    functions

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -4,3 +4,4 @@ echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#" #count
 echo "Script name: $0"
 echo "Current working directory:$PWD"
+echo "Home directory of current user: $HOME"
